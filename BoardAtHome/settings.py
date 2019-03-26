7c4742f68dfbd402882eb1fda78fc967d626145f
Julius <julius@eduroamprvnat-172-16-55-101.near.illinois.edu>
2019-03-26 15:45:57 -0500

    Added login/logout button depending on state

--- BoardAtHome/settings.py
+++ BoardAtHome/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'BoardAtHome.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,4 @@ STATICFILES_DIR =[
 ]
 
 LOGIN_REDIRECT_URL = "/accounts/"
+LOGOUT_REDIRECT_URL = "/accounts/"
--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     url(r'^$',views.home),
     url('login/', LoginView.as_view(template_name='accounts/login.html'), name="login"),
-    url('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name="login"),
+    url('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name="logout"),
     url('register/', views.register, name = "register")
 ]
     #url(r'^login/$', loginView, {{'template_name':'accounts/login.html'}})
