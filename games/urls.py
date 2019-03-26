5b74f2a9939ec795cbee5d90426ffab1be4758ef
tristancinquin <tristan.cinquin@yahoo.fr>
2019-03-26 17:24:36 -0500

    Add files via upload

--- games/urls.py
+++ games/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-
+    url(r'^(?P<game_id>[0-9]+)/$', views.detail, name="detail")
 ]
