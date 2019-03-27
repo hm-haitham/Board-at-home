417c27706097cc96e20079fe571a597825348850
tristancinquin <tristan.cinquin@yahoo.fr>
2019-03-26 22:39:00 -0500

    Update urls.py

--- games/urls.py
+++ games/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls import url
+from . import views
+from django.contrib.auth.views import LoginView, LogoutView
+
 from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^(?P<game_id>[0-9]+)/$', views.detail, name="detail")
 ]
+
