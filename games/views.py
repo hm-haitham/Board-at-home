5c85f4c6d037331ddc44681bd9536f227d6b2c2c
tristancinquin <tristan.cinquin@yahoo.fr>
2019-03-27 09:53:06 -0500

    Update views.py

--- games/views.py
+++ games/views.py
@@ -5,6 +5,10 @@ from django.shortcuts import render
 from django.db import connection
 from django.urls import resolve
 import unicodedata
+from .forms import NameForm
+from django.http import HttpResponseRedirect
+
+
 
 def dictfetchall(cursor):
     "Return all rows from a cursor as a dict"
@@ -18,20 +22,12 @@ def get_games_sql():
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM games WHERE Name NOT NULL;")
         games_rows = dictfetchall(cursor)	#[{'Game_ID': 1, 'Description': "...", Image:"...", ...}, {'Game_ID': 2, 'Description': "...", Image:"..."}...]
-        # cursor.execute("SELECT * FROM Users;")
-        # user_rows = dictfetchall(cursor)
-        # cursor.execute("SELECT * FROM Relation;")
-        # relation_rows = dictfetchall(cursor)
     return games_rows
 
 def get_game_sql(game_id):
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM games WHERE Game_ID=" + str(game_id) + ";")
         game_rows = dictfetchall(cursor)	#[{'Game_ID': 1, 'Description': "...", Image:"...", ...}, {'Game_ID': 2, 'Description': "...", Image:"..."}...]
-        # cursor.execute("SELECT * FROM Users;")
-        # user_rows = dictfetchall(cursor)
-        # cursor.execute("SELECT * FROM Relation;")
-        # relation_rows = dictfetchall(cursor)
     return game_rows[0]
 
 def remove_accents(input_str):
@@ -49,14 +45,45 @@ def detail(request, game_id):
         raise Http404("Game does not exist")
     return render(request, "Game/detail.html", {'game': game, 'user': request.user})
 
-def add_to_list(request):
-    game_id = request.path.split('/')[2].strip()
-    if 'add_wishlist' in request.POST:
-        with connection.cursor() as cursor:
-            cursor.execute("INSERT INTO Relation(Game_ID, User_id, Wishlist) VALUES("
-                           + str(game_id) + "," + str(request.user) + ", True);")
-    if 'add_ownedlist' in request.POST:
-        with connection.cursor() as cursor:
-            cursor.execute("INSERT INTO Relation(Game_ID, User_id, Owned) VALUES("
-                           + str(game_id) + "," + str(request.user) + ", True);")
-    return render(request, "Game/detail.html")
+def get_game_user_relation(user_id, game_id):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM Relation WHERE Game_ID=" + str(game_id) + " AND User_ID=" + str(user_id) +";")
+        game_rows = dictfetchall(cursor)	#[{'Game_ID': 1, 'Description': "...", Image:"...", ...}, {'Game_ID': 2, 'Description': "...", Image:"..."}...]
+    return game_rows
+
+def add_to_wishlist(request, game_id):
+    print (request.user.id)
+    game = get_game_sql(game_id)
+    print request.META.get('HTTP_REFERER')
+    with connection.cursor() as cursor:
+        result = get_game_user_relation(request.user.id, game_id)
+        if not result:
+            cursor.execute("INSERT INTO Relation(Game_ID, User_ID, Wishlist) VALUES("
+                               + str(game_id) + "," + str(request.user.id) + ", True);")
+        else:
+            cursor.execute("UPDATE Relation SET Wishlist=true WHERE User_ID="
+                           + str(request.user.id) + " AND Game_ID="+str(game_id))
+        print("Succes")
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM Relation;")
+        game_rows = dictfetchall(cursor)
+        print(game_rows)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'), {'game': game, 'user': request.user})
+
+def add_to_ownedlist(request, game_id):
+    print (request.user.id)
+    game = get_game_sql(game_id)
+    with connection.cursor() as cursor:
+        result = get_game_user_relation(request.user.id, game_id)
+        if not result:
+            cursor.execute("INSERT INTO Relation(Game_ID, User_ID, Owned) VALUES("
+                           + str(game_id) + "," + str(request.user.id) + ", True);")
+        else:
+            cursor.execute("UPDATE Relation SET Owned=true WHERE User_ID="
+                           + str(request.user.id) + " AND Game_ID=" + str(game_id))
+        print("Succes")
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM Relation;")
+        game_rows = dictfetchall(cursor)
+        print(game_rows)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'), {'game': game, 'user': request.user})
