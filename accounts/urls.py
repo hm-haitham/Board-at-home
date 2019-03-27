b225e1ff891ff761e4e933a743b505892d1c0c78
tristancinquin <tristan.cinquin@yahoo.fr>
2019-03-27 16:55:10 -0500

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
-
 urlpatterns = [
     url(r'^$',views.home),
     url('login/', LoginView.as_view(template_name='accounts/login.html'), name="login"),
@@ -11,6 +10,8 @@ urlpatterns = [
     url('search/', views.search, name = "search"),
     url('about/', views.about, name = 'about'),
     url(r'^profile/$',views.view_profile, name ="view_profile"),
+    url(r'^profile/remove_wishlist/(?P<game_id>[0-9]+)$',views.remove_from_wishlist, name ="remove_from_wishlist"),
+    url(r'^profile/remove_ownedlist/(?P<game_id>[0-9]+)$',views.remove_from_ownedlist, name ="remove_from_ownedlist"),
     url(r'^profile/edit/$',views.edit_profile, name ="edit_profile")
 ]
     #url(r'^login/$', loginView, {{'template_name':'accounts/login.html'}})
