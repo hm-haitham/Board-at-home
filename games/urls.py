8a21ce59a342a95ccba1049ada7a633771c8bfe2
tristancinquin <tristan.cinquin@yahoo.fr>
2019-03-27 09:52:44 -0500

    Update urls.py

--- games/urls.py
+++ games/urls.py
@@ -3,10 +3,13 @@ from django.contrib import admin
 from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import LoginView, LogoutView
-
 from . import views
+from django.views.generic import RedirectView
+
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^(?P<game_id>[0-9]+)/$', views.detail, name="detail")
+    url(r'^(?P<game_id>[0-9]+)/$', views.detail, name="detail"),
+    url(r'^(?P<game_id>[0-9]+)/add_wishlist/$', views.add_to_wishlist, name="add_to_wishlist"),
+    url(r'^(?P<game_id>[0-9]+)/add_owned/$', views.add_to_ownedlist, name="add_to_ownedlist")
 ]
 
