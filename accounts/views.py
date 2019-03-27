98d0735235c785da622b519b507960b65f2a841a
tristancinquin <tristan.cinquin@yahoo.fr>
2019-03-27 10:08:55 -0500

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,59 @@ from django.contrib.auth.forms import UserChangeForm
 # Create your views here.
 
 
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+
+# Retreive the names of the Games in wishlist from database
+def view_wishlist(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT Name FROM Relation Where Wishlist=true and User_ID=" + str(request.user.id))
+        result = dictfetchall(cursor)
+    return result
+
+# Retreive the names of the Games in wishlist from database
+def view_ownedlist(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT Name FROM Relation Where Owned=true and User_ID=" + str(request.user.id))
+        result = dictfetchall(cursor)
+    return result
+
+def get_game_sql(game_id):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM games WHERE Game_ID=" + str(game_id) + ";")
+        game_rows = dictfetchall(cursor)	#[{'Game_ID': 1, 'Description': "...", Image:"...", ...}, {'Game_ID': 2, 'Description': "...", Image:"..."}...]
+    return game_rows[0]
+
+# Remove the names of the Games in wishlist from database
+def remove_from_wishlist(request, game_id):
+    with connection.cursor() as cursor:
+        result = get_game_sql(game_id)
+        if result["Owned"]:
+            cursor.execute("UPDATE Relation SET Wishlist=false WHERE User_ID="
+                           + str(request.user.id) + " AND Game_ID=" + str(game_id))
+        else:
+            cursor.execute("DELETE FROM Relation Where Game_ID=" + str(game_id) +
+                           " and User_ID=" + str(request.user.id))
+    return result
+
+# Remove the names of the Games in wishlist from database
+def view_ownedlist(request):
+    with connection.cursor() as cursor:
+        result = get_game_sql(game_id)
+        if result["Wishlist"]:
+            cursor.execute("UPDATE Relation SET Owned=false WHERE User_ID="
+                           + str(request.user.id) + " AND Game_ID=" + str(game_id))
+        else:
+            cursor.execute("DELETE FROM Relation Where Game_ID=" + str(game_id) +
+                           " and User_ID=" + str(request.user.id))
+    return result
+
+
 def home(request):
     numbers = [1,2,3,4,5]
     name = "Julius Pasion"
